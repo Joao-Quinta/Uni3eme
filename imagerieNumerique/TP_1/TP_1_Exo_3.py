1ed270b582049f821f706f45e823e2ebd3412dd4
Joao-Quinta <qjoao.filipe@gmail.com>
2020-10-06 20:49:23 +0200

    Update TP_1_Exo_3.py

--- imagerieNumerique/TP_1/TP_1_Exo_3.py
+++ imagerieNumerique/TP_1/TP_1_Exo_3.py
@@ -1,16 +1,9 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+import skimage.color as ski
 
-
-x = np.linspace(0, 1, 100)
-
-image = np.tile(x, (25, 1)).T
-print(image)
-print(len(image))
-print(len(image[0]))
-print(image[0][0])
-print(image[0][24])
-print(image[99][0])
-plt.imshow(image, cmap='gray')
+lena = mpimg.imread("lena.png")
+grayscale = ski.rgb2gray(lena)
+plt.imshow(grayscale)
 plt.show()
