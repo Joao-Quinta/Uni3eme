3899030f7e833dbc619d5612a5591582acc0a6c4
Joao-Quinta <qjoao.filipe@gmail.com>
2020-10-06 16:09:36 +0200

    Update imagerieExo1.py

--- imagerieNumerique/latex_IN_TP_1/imagerieExo1.py
+++ imagerieNumerique/latex_IN_TP_1/imagerieExo1.py
@@ -3,7 +3,8 @@ import matplotlib.image as mpimg
 from PIL import Image
 
 lena = mpimg.imread("lena.png")
-print(lena)
 print(len(lena))
-lenaPlot = plt.imshow(lena)
+print(len(lena[0]))
+print(lena[0][0])
+plt.imshow(lena)
 
