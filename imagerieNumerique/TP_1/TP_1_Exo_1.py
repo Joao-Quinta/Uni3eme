6bd272851636c8d2ed02b35c99ead23272eb95b7
Joao-Quinta <qjoao.filipe@gmail.com>
2020-10-06 17:51:47 +0200

    pain

--- imagerieNumerique/TP_1/TP_1_Exo_1.py
+++ imagerieNumerique/TP_1/TP_1_Exo_1.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-lena = mpimg.imread("lena.png")#lecture de image (ouvre l image)
-plt.imshow(lena)#ca va afficher l image, mais ca a besoin de la ligne 6
+lena = mpimg.imread("lena.png")
+plt.imshow(lena)
 plt.colorbar()
-plt.show()#sans cette ligne l image s affiche aps
+plt.show()
 
