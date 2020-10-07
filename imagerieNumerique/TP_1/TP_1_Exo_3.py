2f1f5ebe40c3714aa0f15eebdbb4854656d21283
Joao-Quinta <qjoao.filipe@gmail.com>
2020-10-07 20:41:58 +0200

    Update TP_1_Exo_3.py

--- imagerieNumerique/TP_1/TP_1_Exo_3.py
+++ imagerieNumerique/TP_1/TP_1_Exo_3.py
@@ -10,6 +10,24 @@ grayscale = ski.rgb2gray(lena)
 # plt.show()
 
 
-cropped_image = skimage.util.crop(grayscale, ((100, 116), (150, 121)), copy=False)
-plt.imshow(cropped_image, cmap='gray')
+cropped_imageA = skimage.util.crop(grayscale, ((100, 116), (150, 121)), copy=False)
+# plt.imshow(cropped_imageA, cmap='gray')
+# plt.show()
+cropped_imageB = grayscale[100:380,150:375]
+
+# plt.imshow(cropped_imageB, cmap='gray')
+# plt.show()
+
+imagesDone = [grayscale,cropped_imageA,cropped_imageB]
+labelImagesDone = ["grayscale","(a)","(b)"]
+rows = 1
+cols = 3
+axes = []
+fig = plt.figure()
+
+for a in range(rows * cols):
+    axes.append(fig.add_subplot(rows, cols, a + 1))
+    plt.imshow(imagesDone[a], cmap='gray')
+    #plt.legend(labelImagesDone[a])
+fig.tight_layout()
 plt.show()
