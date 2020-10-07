0e44fc4a42de60948df1f0bcd85420af3ef7dfd3
Joao-Quinta <qjoao.filipe@gmail.com>
2020-10-07 20:29:47 +0200

    Update TP_1_Exo_3.py

--- imagerieNumerique/TP_1/TP_1_Exo_3.py
+++ imagerieNumerique/TP_1/TP_1_Exo_3.py
@@ -2,8 +2,14 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import skimage.color as ski
+import skimage
 
 lena = mpimg.imread("lena.png")
 grayscale = ski.rgb2gray(lena)
-plt.imshow(grayscale)
+# plt.imshow(grayscale, cmap='gray')
+# plt.show()
+
+
+cropped_image = skimage.util.crop(grayscale, ((100, 116), (150, 121)), copy=False)
+plt.imshow(cropped_image, cmap='gray')
 plt.show()
