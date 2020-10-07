f34fccb003d2dafd537a06fe3bd08576dc67f5b8
Joao-Quinta <qjoao.filipe@gmail.com>
2020-10-07 14:20:27 +0200

    Update TP_1_Exo_1.py

--- imagerieNumerique/TP_1/TP_1_Exo_1.py
+++ imagerieNumerique/TP_1/TP_1_Exo_1.py
@@ -1,8 +1,23 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import copy
 
-lena = mpimg.imread("lena.png")
-plt.imshow(lena)
-plt.colorbar()
+
+def transformImage(images):
+    for j in range(0, len(images[0])):
+        for z in range(0, len(images[0][j])):
+            for t in range(0, len(images)):
+                for i in range(0, len(images[0][j][z])):
+                    if i != t:
+                        images[t][j][z][i] = 0
+    return images
+
+
+lenaRGB = mpimg.imread("lena.png")
+lenaR = copy.deepcopy(lenaRGB)
+lenaG = copy.deepcopy(lenaRGB)
+lenaB = copy.deepcopy(lenaRGB)
+[lenaR, lenaG, lenaB] = transformImage([lenaR, lenaG, lenaB])
+
+fig, axes = plt.subplots(nrows=2, ncols=2)
 plt.show()
-
