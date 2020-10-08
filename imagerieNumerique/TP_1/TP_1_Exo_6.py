a41423090e1cf832918a32b4850df996b60c5e1c
Joao-Quinta <qjoao.filipe@gmail.com>
2020-10-08 19:37:20 +0200

    Update TP_1_Exo_6.py

--- imagerieNumerique/TP_1/TP_1_Exo_6.py
+++ imagerieNumerique/TP_1/TP_1_Exo_6.py
@@ -2,4 +2,18 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import skimage.color as ski
+import skimage.util as util
+from scipy import ndimage
 
+# load the image and turn it to grayscale
+lena = mpimg.imread("lena.png")
+grayscale = ski.rgb2gray(lena)
+
+grayscaleMean = np.mean(grayscale)
+grayscaleVariance = np.var(grayscale)
+
+grayscaleMeans = ndimage.mean(grayscale)
+grayscaleVariances = ndimage.variance(grayscale)
+
+print(grayscaleMean, grayscaleVariance)
+print(grayscaleMeans, grayscaleVariances)
