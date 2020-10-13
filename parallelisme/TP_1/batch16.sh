45d2b4feb5f70d9442e389428fc7d2cbd2aa25ca
Joao-Quinta <qjoao.filipe@gmail.com>
2020-10-13 14:11:25 +0200

    batch

---
+++ parallelisme/TP_1/batch16.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=bCast
+#SBATCH --ntasks=16
+#SBATCH --time=0-00:03:00
+#SBATCH --mail-user=Joao.Costa@etu.unige.ch
+#SBATCH --partition=debug-EL7
+#SBATCH --output=outBcast16.out
+#SBATCH --constraint=E5-2660V0
+
+srun ./hello
