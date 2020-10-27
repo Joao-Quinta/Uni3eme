22616263eb4bc41b3ca6c88111ea5bac03eae622
Joao Filipe Costa Da Quinta <qjoao.filipe@gmail.com>
2020-10-27 21:46:32 +0100

    s

--- parallelisme/TP_2/heat_p.cc
+++ parallelisme/TP_2/heat_p.cc
@@ -20,9 +20,9 @@ void save(Array2D<double> &matrix, std::string name) {
 
 int main(int argc, char **argv) {
 
-  const int dimX = 200;
-  const int dimY = 100;
-  const int maxT = 40000;
+  const int dimX = 7;
+  const int dimY = 6;
+  const int maxT = 4000;
 
   int myRank, nProc;
   std::vector<int> sizes;
@@ -31,84 +31,103 @@ int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
   MPI_Comm_size(MPI_COMM_WORLD, &nProc);
-  std::cout << "I'm process " << myRank << " and there are "<< nProc << " procs" <<std::endl;
 
-  // y a que le processus de rank 0 qui cree les matrices au debut
-  if (myRank == 0){
-    Array2D<double> heat(dimX, dimY, 0); // La matrice de la chaleur
-    Array2D<double> tmp(dimX, dimY, 0);  // Une matrice temporaire
+  Array2D<double> heat(dimX, dimY, 0); // La matrice de la chaleur
+  Array2D<double> tmp(dimX, dimY, 0);  // Une matrice temporaire
 
-    for (int iX=0; iX<dimX; iX++) {      // conditions aux bords:
-        heat(iX,0) = 0;                 // 0 en haut
-        heat(iX,dimY-1) = 1;            // 1 en bas
-        tmp(iX,0) = 0;                  // 0 en haut
-        tmp(iX,dimY-1) = 1;             // 1 en bas
-    }
-    for (int iY=0; iY<dimY; iY++) {
-        heat(0,iY)      = 0.;           // 0 a gauche
-        heat(dimX-1,iY) = 1.;           // 1 a droite
-        tmp(0,iY)      = 0.;            // 0 a gauche
-        tmp(dimX-1,iY) = 1.;            // 1 a droite
-    }
+  for (int iX=0; iX<dimX; iX++) {      // conditions aux bords:
+      heat(iX,0) = 0;                 // 0 en haut
+      heat(iX,dimY-1) = 1;            // 1 en bas
+      tmp(iX,0) = 0;                  // 0 en haut
+      tmp(iX,dimY-1) = 1;             // 1 en bas
   }
-  // ca marche presque comme voulu lole :
-  // nLinesProc = nombre de lignes par processus, ceil arrondit cette valeur en haut
-  // exemple : 100 lignes en 3 proc, ceil(100/3) = 34
-  // on met 34 lignes au premier (reste: 66)
-  // on redivise 66 par 2 mtn -> 33, ceil(66/2) = 33
-  // on met donc 33 a chaque processus restant
+  for (int iY=0; iY<dimY; iY++) {
+      heat(0,iY)      = 0.;           // 0 a gauche
+      heat(dimX-1,iY) = 1.;           // 1 a droite
+      tmp(0,iY)      = 0.;            // 0 a gauche
+      tmp(dimX-1,iY) = 1.;            // 1 a droite
+  }
 
-  // sizes ressemble donc a :    {34,33,33}
-  // displacements ressemble a : {0,34,67}
-
-  // procSansL est le nb de proc sans lignes assignes
   int procSansL = nProc;
-  // nb de lignes qu il restent a assigner
   int nLinesRestantes = dimY;
-  // on assigne au premier proc dimY/nProc, et on arrondit en haut
   int nLinesProc = ceil(nLinesRestantes/procSansL);
 
-  // std::vector<int> size;
-  // std::vector<int> displacements;
-  // on construit donc les deux vecteurs
 
   while (nLinesRestantes > 0) {
     if (displacements.size() == 0){
-      // si premiere iteration -> displacements est vide, donc on push 0
       displacements.push_back(0);
     } else{
-      // si pa 1ere itération -> dsplacements push les derniers elements de displacements + size
       displacements.push_back(displacements.back() + sizes.back());
     }
     sizes.push_back(nLinesProc);
-    // on a assigne des lignes a un proc, donc on remet a jour les valeurs
     nLinesRestantes = nLinesRestantes - nLinesProc;
     procSansL = procSansL - 1;
-    // on recalcule lignes par proc -> si on veut donner 100 a 3 ->
-    // on fait ainsi {34,33,33} plutot que {34,34,32}
     if (procSansL > 0){
       nLinesProc = ceil(nLinesRestantes/procSansL);
     }
   }
-  
-  /*
+  Array2D<double> heatReceive(dimX, sizes[myRank], 0); // La matrice de la chaleur
+  Array2D<double> tmpReceive(dimX, sizes[myRank], 0);  // Une matrice temporaire
+
+  int tailleBufferRecu = dimX * sizes[myRank];
+
+  //MPI_Scatterv(&heat, sizes.data(), displacements.data(), MPI_DOUBLE, &heatReceive,tailleBufferRecu, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+  //std::cout << "I'm process " << myRank << " and there are "<< nProc << " procs" <<std::endl;
   if (myRank == 0){
-    for (int i = 0; i < displacements.size(); i++){
-      printf("%d - ", displacements[i]);
+    for (int i = 0; i<dimY; i++){
+      for(int j = 0; j < dimX; j++){
+        printf(" %f ", heat(j,i));
+      }printf("\n" );
     }
-    printf("\n");
-    for (int i = 0; i < displacements.size(); i++){
-      printf("%d - ", sizes[i]);
-    }
-    printf("\n");
   }
-  */
+  // printf("my rank is %d  and my displacements is %d \n", myRank, displacements[myRank]);
+  // print elements recus par chaque
 
+  if(myRank > 0){
+    MPI_Status status;
+    int b;
+    MPI_Recv(&b, 1, MPI_INT, myRank-1, 0, MPI_COMM_WORLD, &status);
+  }
 
+  for (int i = displacements[myRank]; i < displacements[myRank] + sizes[myRank]; i++){
+    //printf("my rank is %d  and my displacements is %d \n", myRank, displacements[myRank]);
+    for (int j = 0; j < dimX; j++){
+      int z = i - displacements[myRank];
+      //printf("heatReceive : (%d , %d) = heat : (%d , %d)\n", j,z,j,i);
+      heatReceive(j,z) = heat(j,i);
+    }
+  }
+  printf(" my rank is : %d\n", myRank);
+  for (int i = 0; i< sizes[myRank]; i++){
+    for(int j = 0; j < dimX; j++){
+      printf(" %f ", heatReceive(j,i));
+    }printf("\n" );
+  }
+  if (myRank<nProc-1){
+    int b = 1;
+    MPI_Send(&b, 1, MPI_INT, myRank+1, 0, MPI_COMM_WORLD);
+  }
 
+  vectorEnvoieTop = std::vector<int>(dimX, 0);
+  vectorRecuTop = std::vector<int>(dimX, 0);
+  vectorEnvoieBot = std::vector<int>(dimX, 0);
+  vectorRecuBot = std::vector<int>(dimX, 0);
 
-  //mtn il faut scatter les matrices (les deux)
-  //MPI_Scatter(&heat,nLines,vector<int>,&heat,0,1);
   MPI_Finalize();
 
 }
+
+/*
+// print displacements and sizes
+if (myRank == 0){
+  for (int i = 0; i < displacements.size(); i++){
+    printf("%d - ", displacements[i]);
+  }
+  printf("\n");
+  for (int i = 0; i < displacements.size(); i++){
+    printf("%d - ", sizes[i]);
+  }
+  printf("\n");
+}
+*/
--- parallelisme/TP_2/test_heat.cc
+++ parallelisme/TP_2/test_heat.cc
@@ -20,9 +20,9 @@ void save(Array2D<double> &matrix, std::string name) {
 
 int main(int argc, char **argv) {
 
-  const int dimX = 200;
-  const int dimY = 100;
-  const int maxT = 40000;
+  const int dimX = 5;
+  const int dimY = 6;
+  const int maxT = 4000;
 
   int myRank, nProc;
   std::vector<int> sizes;
@@ -31,51 +31,114 @@ int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
   MPI_Comm_size(MPI_COMM_WORLD, &nProc);
-  std::cout << "I'm process " << myRank << " and there are "<< nProc << " procs" <<std::endl;
+  // std::cout << "I'm process " << myRank << " and there are "<< nProc << " procs" <<std::endl;
 
-  if (myRank == 0){
-    Array2D<double> heat(dimX, dimY, 0); // La matrice de la chaleur
-    Array2D<double> tmp(dimX, dimY, 0);  // Une matrice temporaire
+  // y a que le processus de rank 0 qui cree les matrices au debut
 
-    for (int iX=0; iX<dimX; iX++) {      // conditions aux bords:
-        heat(iX,0) = 0;                 // 0 en haut
-        heat(iX,dimY-1) = 1;            // 1 en bas
-        tmp(iX,0) = 0;                  // 0 en haut
-        tmp(iX,dimY-1) = 1;             // 1 en bas
-    }
-    for (int iY=0; iY<dimY; iY++) {
-        heat(0,iY)      = 0.;           // 0 a gauche
-        heat(dimX-1,iY) = 1.;           // 1 a droite
-        tmp(0,iY)      = 0.;            // 0 a gauche
-        tmp(dimX-1,iY) = 1.;            // 1 a droite
-    }
+  Array2D<double> heat(dimX, dimY, 0); // La matrice de la chaleur
+  Array2D<double> tmp(dimX, dimY, 0);  // Une matrice temporaire
+
+  for (int iX=0; iX<dimX; iX++) {      // conditions aux bords:
+      heat(iX,0) = 0;                 // 0 en haut
+      heat(iX,dimY-1) = 1;            // 1 en bas
+      tmp(iX,0) = 0;                  // 0 en haut
+      tmp(iX,dimY-1) = 1;             // 1 en bas
   }
-  if (myRank == 0){
+  for (int iY=0; iY<dimY; iY++) {
+      heat(0,iY)      = 0.;           // 0 a gauche
+      heat(dimX-1,iY) = 1.;           // 1 a droite
+      tmp(0,iY)      = 0.;            // 0 a gauche
+      tmp(dimX-1,iY) = 1.;            // 1 a droite
+  }
+
+  // heat = { {1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16} };
+  // ca marche presque comme voulu lole :
+  // nLinesProc = nombre de lignes par processus, ceil arrondit cette valeur en haut
+  // exemple : 100 lignes en 3 proc, ceil(100/3) = 34
+  // on met 34 lignes au premier (reste: 66)
+  // on redivise 66 par 2 mtn -> 33, ceil(66/2) = 33
+  // on met donc 33 a chaque processus restant
+
+  // sizes ressemble donc a :    {34,33,33}
+  // displacements ressemble a : {0,34,67}
+
+  // procSansL est le nb de proc sans lignes assignes
   int procSansL = nProc;
+  // nb de lignes qu il restent a assigner
   int nLinesRestantes = dimY;
+  // on assigne au premier proc dimY/nProc, et on arrondit en haut
   int nLinesProc = ceil(nLinesRestantes/procSansL);
 
+  // std::vector<int> size;
+  // std::vector<int> displacements;
+  // on construit donc les deux vecteurs
+
   while (nLinesRestantes > 0) {
-    printf("numero de processeur sans ligne : %d || nLines par proc : %d || numero lignes restantes : %d \n", procSansL, nLinesProc, nLinesRestantes);
     if (displacements.size() == 0){
+      // si premiere iteration -> displacements est vide, donc on push 0
       displacements.push_back(0);
     } else{
-      printf(" diplacement last : %d || size last : %d \n", displacements.back(),sizes.back());;
+      // si pa 1ere itération -> dsplacements push les derniers elements de displacements + size
       displacements.push_back(displacements.back() + sizes.back());
-      printf(" addition : %d\n", displacements.back());
     }
     sizes.push_back(nLinesProc);
+    // on a assigne des lignes a un proc, donc on remet a jour les valeurs
     nLinesRestantes = nLinesRestantes - nLinesProc;
     procSansL = procSansL - 1;
+    // on recalcule lignes par proc -> si on veut donner 100 a 3 ->
+    // on fait ainsi {34,33,33} plutot que {34,34,32}
     if (procSansL > 0){
       nLinesProc = ceil(nLinesRestantes/procSansL);
+    }
   }
-  printf("size 0 : %d ||  displacement 0 : %d  \n", sizes[0], displacements[0]);
-  }
-  printf("%d || %d || %d || %d \n", sizes[0], sizes[1], sizes[2], sizes[3]);
-  printf("%d || %d || %d || %d \n", displacements[0], displacements[1], displacements[2], displacements[3]);
+  Array2D<double> heatReceive(dimX, sizes[myRank], 0); // La matrice de la chaleur
+  Array2D<double> tmpReceive(dimX, sizes[myRank], 0);  // Une matrice temporaire
 
-}
+  /*
+  if (myRank == 0){
+    for (int i = 0; i < displacements.size(); i++){
+      printf("%d - ", displacements[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < displacements.size(); i++){
+      printf("%d - ", sizes[i]);
+    }
+    printf("\n");
+  }
+  */
+
+  int tailleBufferRecu = dimX * sizes[myRank];
+
+  MPI_Scatterv(&heat, sizes.data(), displacements.data(), MPI_DOUBLE, &heatReceive,tailleBufferRecu, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+
+  //std::cout << "I'm process " << myRank << " and there are "<< nProc << " procs" <<std::endl;
+  if (myRank == 0){
+    for (int i = 0; i<dimX; i++){
+      for(int j = 0; j < dimY; j++){
+        printf(" %f ", heat(i,j));
+      }printf("\n" );
+    }
+  }
+
+  if(myRank > 0){
+    MPI_Status status;
+    int b;
+    MPI_Recv(&b, 1, MPI_INT, myRank-1, 0, MPI_COMM_WORLD, &status);
+  }
+
+  printf("my rank is : %d\n", myRank);
+  for (int i = 0; i<dimX; i++){
+    for(int j = 0; j < sizes[myRank]; j++){
+      printf(" %f ", heatReceive(i,j));
+    }printf("\n" );
+  }
+
+  if (myRank<nProc-1){
+    int b = 1;
+    MPI_Send(&b, 1, MPI_INT, myRank+1, 0, MPI_COMM_WORLD);
+  }
+
 
 
   //mtn il faut scatter les matrices (les deux)
