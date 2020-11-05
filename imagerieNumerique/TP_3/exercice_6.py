e82482ad85a879d13be9f8a5a3a564687ef8af30
Joao-Quinta <56771749+Joao-Quinta@users.noreply.github.com>
2020-11-05 22:55:58 +0100

    s

--- imagerieNumerique/TP_3/exercice_6.py
+++ imagerieNumerique/TP_3/exercice_6.py
@@ -25,31 +25,24 @@ def psnr(image1, image2):
 
 
 # (a)
-
-
 reference = plt.imread("reference.bmp")
 noisy = plt.imread("noisy.bmp")
 
 rR, rG, rB = reference[:, :, 0], reference[:, :, 1], reference[:, :, 2]
 nR, nG, nB = noisy[:, :, 0], noisy[:, :, 1], noisy[:, :, 2]
 allImages = [reference, rR, rG, rB, noisy, nR, nG, nB]
+allImagesGray = []
 for i in range(0, len(allImages)):
-    allImages[i] = ski.rgb2gray(allImages[i])
+    allImagesGray.append(ski.rgb2gray(allImages[i]))
 
 psnrL = []
-for i in range(0, int(len(allImages) / 2)):
-    psnrL.append(psnr(allImages[i], allImages[i + 4]))
-
-print(psnrL)
-
-'''
-
-all in grayscale:
-
-    ORIGINAL             RED                GREEN               BLUE
-[66.668112357439, 13.278653649294078, 15.935008236845567, 10.261868583985915]
-
-'''
+for i in range(0, int(len(allImagesGray) / 2)):
+    psnrL.append(psnr(allImagesGray[i], allImagesGray[i + 4]))
+avgPSNR = 0
+for i in range(1, len(psnrL)):
+    avgPSNR = avgPSNR + psnrL[i]
+avgPSNR = avgPSNR / 3
+print("the avg PSNR in exercice A is : ", avgPSNR, " dB")
 
 allLabels = ["original", "red", "green", "blue", "noisy - PSNR : " + str(psnrL[0]),
              "noisy Red - PSNR : " + str(psnrL[1]), "noisy Green - PSNR : " + str(psnrL[2]),
@@ -61,13 +54,13 @@ fig = plt.figure()
 
 for a in range(rows * cols):
     axes.append(fig.add_subplot(rows, cols, a + 1))
-    plt.imshow(allImages[a], cmap='gray')
+    plt.imshow(allImagesGray[a], cmap='gray')
     plt.title(allLabels[a])
 plt.show()
 
 # (b)
-real = [rR, rG, rB]
-x = [nR, nG, nB]
+real = [allImages[1], allImages[2], allImages[3]]
+x = [allImages[5], allImages[6], allImages[7]]
 y = []
 z = []
 for i in range(0, len(x)):
@@ -75,10 +68,26 @@ for i in range(0, len(x)):
     z.append(spy.zoom(spy.zoom(y[i], (2.0, 2.0), order=0), (2.0, 2.0), order=0))
 
 psnrLL = []
-for i in range(0,len(real)):
+for i in range(0, len(real)):
     psnrLL.append(psnr(real[i], z[i]))
 
-# [13.278653649294078, 15.935008236845567, 10.261868583985915]
-print(psnrL)
-# [17.82404318535903, 21.53364625460226, 14.686179862808075]
-print(psnrLL)
+avgPSNRB = 0
+for i in range(0, len(psnrLL)):
+    avgPSNRB = avgPSNRB + psnrLL[i]
+
+avgPSNRB = avgPSNRB / 3
+print("the avg PSNR in exercice B is : ", avgPSNRB, " dB")
+# (c)
+realGray = [allImagesGray[0]]
+xGray = [allImagesGray[4]]
+yGray = []
+zGray = []
+for i in range(0, len(xGray)):
+    yGray.append(transform.downscale_local_mean(transform.downscale_local_mean(xGray[i], (2, 2)), (2, 2)))
+    zGray.append(spy.zoom(spy.zoom(yGray[i], (2.0, 2.0), order=0), (2.0, 2.0), order=0))
+
+psnrLLL = []
+for i in range(0, len(realGray)):
+    psnrLLL.append(psnr(realGray[i], zGray[i]))
+print("the PSNR in exercice C is : ", psnrLLL[0], " dB")
+
