013d923c1aeea2adf93c8cc5e2ef13b30db692a9
Joao Filipe Costa Da Quinta <qjoao.filipe@gmail.com>
2020-12-07 16:38:51 +0100

    ss

--- parallelisme/tp2_assistant/no_seq/script.sh
+++ parallelisme/tp2_assistant/no_seq/script.sh
@@ -1,13 +1,13 @@
-#!/bin/sh 
+#!/bin/sh
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name=laplace
 #SBATCH --ntasks=1
 #SBATCH --time=0-00:20:00
 #SBATCH --mail-user=Joao.Costa@etu.unige.ch
 #SBATCH --mail-type=END
-#SBATCH --partition=debug
+#SBATCH --partition=debug-EL7
 #SBATCH --clusters=baobab
-#SBATCH --output=slurm-%J.out
+#SBATCH --output=teste.out
 
 
-srun your_binary
+srun ./laplace 2000 2000 500 tes
